990a968a80ab83308022a853fb3cb0a5c2d348e9
Mohamed <mohamedbentouhami512@gmail.com>
2025-04-22 11:27:27 +0200

    add config to connect to mysql db

--- src/main/java/com/example/e_commerce_api/models/Category.java
+++ src/main/java/com/example/e_commerce_api/models/Category.java
@@ -1,5 +1,16 @@
 package com.example.e_commerce_api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
-    
+    @Id
+    private long id;
 }
--- src/main/java/com/example/e_commerce_api/models/Profile.java
+++ src/main/java/com/example/e_commerce_api/models/Profile.java
@@ -1,5 +1,17 @@
 package com.example.e_commerce_api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
-    
+    @Id
+    private long id;
+
 }
