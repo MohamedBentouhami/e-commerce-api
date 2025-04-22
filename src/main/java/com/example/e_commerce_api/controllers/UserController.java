5e44dd3bf63ca7e02423a8de2654c5acbf934168
Mohamed <mohamedbentouhami512@gmail.com>
2025-04-23 01:08:14 +0200

    add restfull user controller

--- src/main/java/com/example/e_commerce_api/controllers/UserController.java
+++ src/main/java/com/example/e_commerce_api/controllers/UserController.java
@@ -4,17 +4,26 @@ import com.example.e_commerce_api.models.User;
 import com.example.e_commerce_api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
